3bc78ea56a8b3beb28b4d35af17460413be5d494
Seun-Ajayi <youoluwaseunaajayi@student.oauife.edu.ng>
2022-08-17 18:25:56 +0100

    added offline files

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -12,7 +12,7 @@ import mlflow
 import json
 
 
-import pandas as pdS
+import pandas as pd
 import numpy as np
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
